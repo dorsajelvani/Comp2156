b15a8611bb675509ccb4f1596e197f13011a6e1c
Dorsa <dor_j76@yahoo.com>
2023-02-08 19:39:31 -0500

    added all package.json and lock and src and public.

---
+++ build/precache-manifest.c9c2612138d979ba5ce9edee899bf4c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c29b1d0e2b6d7d870c2",
+    "url": "/dorsajelvani/Comp2156/static/css/main.9ae73b83.chunk.css"
+  },
+  {
+    "revision": "4c29b1d0e2b6d7d870c2",
+    "url": "/dorsajelvani/Comp2156/static/js/main.4c29b1d0.chunk.js"
+  },
+  {
+    "revision": "d61645abecb340cfbb63",
+    "url": "/dorsajelvani/Comp2156/static/js/runtime~main.d61645ab.js"
+  },
+  {
+    "revision": "64d6decb088646398d8f",
+    "url": "/dorsajelvani/Comp2156/static/js/2.64d6decb.chunk.js"
+  },
+  {
+    "revision": "4cf871e7449c63bce9cde6f85989f49b",
+    "url": "/dorsajelvani/Comp2156/index.html"
+  }
+];
